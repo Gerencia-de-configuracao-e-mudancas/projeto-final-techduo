9784dd41e4fa48c1e5947dd02827bc06eca5a52c
Maria Eloisa <mariaeloisavalenca@gmail.com>
2023-12-08 15:26:07 -0300

    Criando arquivo js para os cards

---
+++ script.js
@@ -0,0 +1,15 @@
+const divCard = document.getElementById("card");
+
+let cardNome = ["Margot Robbie", "Ryan Gosling", "America Ferrera", "Ariana Greenbalt", "Will Farrell", "Emma McKey", "Issa Rae", "Michael Cera", "Simu Liu"];
+let cardFoto = ["https://br.web.img3.acsta.net/c_310_420/pictures/23/07/17/19/00/0289166.jpg", "https://br.web.img3.acsta.net/c_310_420/pictures/16/05/17/17/28/208580.jpg", "https://br.web.img3.acsta.net/c_310_420/pictures/16/01/26/11/12/335396.jpg", "https://br.web.img3.acsta.net/c_310_420/pictures/23/09/18/15/23/2403096.jpg", 
+"https://br.web.img3.acsta.net/c_310_420/pictures/14/09/06/17/37/169130.jpg",					"https://br.web.img3.acsta.net/c_310_420/pictures/19/01/21/23/25/1928969.jpg", "https://br.web.img3.acsta.net/c_310_420/pictures/18/09/12/19/36/1848742.jpg", "https://br.web.img3.acsta.net/c_310_420/pictures/18/09/19/22/37/5221873.jpg", "https://br.web.img2.acsta.net/c_310_420/pictures/19/07/21/14/42/1468199.jpg"];
+
+for(let i=0; i<cardNome.length; i++){
+    let nome = cardNome[i];
+    let foto = cardFoto[i];
+    let card = `<div class="cardConteiner">
+    <img src="${foto}">
+    <h3>${nome}</h3>
+    </div>`;
+    divCard.innerHTML += card;
+}
